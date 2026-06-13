3020f924deaff775cb23374b1e6cb06ed5b4ed1a
compiler-for-languages <centamillenium@gmail.com>
2026-06-13 08:26:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/infotact/hotel_reservation_engine/HotelReservationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.infotact.hotel_reservation_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationEngineApplication.class, args);
+	}
+
+}
