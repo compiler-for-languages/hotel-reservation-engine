d21e7ff8f0a278f9865bd7d2f926189eeaafbd69
Spandana V <spandanavadigeri@gmail.com>
2026-06-18 22:10:02 +0530

    feat: Added AssignmentStatus enum values for status tracking(fixes #7)

--- Backend/project1/src/main/java/com/infotact/project1/enums/AssignmentStatus.java
+++ Backend/project1/src/main/java/com/infotact/project1/enums/AssignmentStatus.java
@@ -1,4 +1,10 @@
 package com.infotact.project1.enums;
 
 public enum AssignmentStatus {
+
+    ACTIVE,//Room assigned
+//Guest currently staying
+     COMPLETED,//Guest checked out
+// Assignment finished
+
 }
