92493316ed3133b28d97662b01eeb72cfba40b72
Spandana <spandanavadigeri@gmail.com>
2026-06-27 23:24:54 +0530

    feat: Updated ReservationRequestDTO for implementing different payment methods

--- Backend/project1/src/main/java/com/infotact/project1/dto/request/ReservationRequestDTO.java
+++ Backend/project1/src/main/java/com/infotact/project1/dto/request/ReservationRequestDTO.java
@@ -3,6 +3,7 @@ package com.infotact.project1.dto.request;
 import lombok.Data;
 
 import java.time.LocalDate;
+import com.infotact.project1.enums.PaymentMethod;
 
 /*
  * Request payload used to create a reservation.
@@ -22,5 +23,8 @@ public class ReservationRequestDTO {
     private Integer guestCount;
 
     private String specialRequest;
+
+    // Customer selected payment method
+    private PaymentMethod paymentMethod;
 
 }
