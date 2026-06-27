45fe5b51730c8b3f049e477b8195bd20bcafbad7
Spandana <spandanavadigeri@gmail.com>
2026-06-27 23:07:11 +0530

    feat: Updated ReservationRequestDTO inorder to add different payment methods ( fixes #43)

--- Backend/project1/src/main/java/com/infotact/project1/dto/request/ReservationRequestDTO.java
+++ Backend/project1/src/main/java/com/infotact/project1/dto/request/ReservationRequestDTO.java
@@ -22,4 +22,6 @@ public class ReservationRequestDTO {
     private Integer guestCount;
 
     private String specialRequest;
+
+    private String paymentMethod; // Customer's selected payment method 
 }
