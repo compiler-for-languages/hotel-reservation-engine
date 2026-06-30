2efdff59b7ef9f5987ca998c699e4f7cb2b00e12
Spandana <spandanavadigeri@gmail.com>
2026-06-30 14:23:27 +0530

    Testing

--- Backend/project1/src/main/java/com/infotact/project1/security/SecurityConfig.java
+++ Backend/project1/src/main/java/com/infotact/project1/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * Central security configuration.
  */
 
+// role based access must be provided !
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
